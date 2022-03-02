382b456709d6a83d6f93587803e1499e2f7e887b
Serguei <serkhas@hotmail.com>
2022-03-02 13:45:36 -0500

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 echo "Publish $(git remote get-url origin) @ $(git rev-parse HEAD)"
-gh-pages -d build -m "Publish $(git remote get-url origin) @ $(git rev-parse HEAD)"
+gh-pages -d dist -m "Publish $(git remote get-url origin) @ $(git rev-parse HEAD)"
